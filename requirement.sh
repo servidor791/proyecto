b7c5c839f4a8a7a5066f86a7406fae89be5cec07
servidor791 <81490654+servidor791@users.noreply.github.com>
2021-03-27 15:30:53 -0600

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,10 @@
+sleep 2
+echo -e "\e[1;33m             _    __ \e[1;36m"
+echo -e "\e[1;33m            | |  / _| \e[1;36m"
+echo -e "\e[1;33m  _ __   ___| |_| |_ _ __ ___  ___ \e[1;36m"
+echo -e "\e[1;33m | '_ \ / _ \ __|  _| '__/ _ \/ _ \ \e[1;36m"
+echo -e "\e[1;33m | | | |  __/ |_| | | | |  __/  __/ \e[1;36m"
+echo -e "\e[1;33m |_| |_|\___|\__|_| |_|  \___|\___| \e[1;36m"
+sleep 2
+git clone https://github.com/servidor791/zzz
+clear
