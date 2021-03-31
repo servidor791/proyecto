7c64676c702c2608a7edf49e122a9fbefdcfe47b
servidor791 <81490654+servidor791@users.noreply.github.com>
2021-03-31 11:12:04 -0600

    Add files via upload

---
+++ mundocracker.sh
@@ -0,0 +1,81 @@
+#!bin/bash
+#creador 
+clear
+sleep 2
+echo -e "\e[1;30m                  ________-----_____"
+sleep 1
+echo -e "\e[1;30m       _____------           __      ----_"
+sleep 1
+echo -e "\e[1;30m___----             ___------              "
+sleep 1
+echo -e "\e[1;30m   ----________        ---- "
+sleep 1
+echo -e "\e[1;30m               -----__    |             _____) "
+sleep 1
+echo -e "\e[1;30m                    __-                /#####/ "
+sleep 1
+echo -e "\e[1;30m        _______-----    ___--          \####/ "
+sleep 1
+echo -e "\e[1;30m  ------_______      ---____            \##/  / "
+echo -e "\e[1;30m               -----__    \ --    _          / "
+sleep 1
+echo -e "\e[1;30m                      --__--__     \_____/   \_/ "
+sleep 1
+echo -e "\e[1;30m                              ----|   /          | "
+sleep 1
+echo -e "\e[1;30m                                  |  |___________| "
+sleep 1
+echo -e "\e[1;30m                                  |  | ((_(_)| )_) "
+sleep 1
+echo -e "\e[1;30m                                  |  \_((_(_)|/(_) "
+sleep 1
+echo -e "\e[1;30m                                  \             ( "
+sleep 1
+echo -e "\e[1;30m                                   \_____________) "
+echo ""
+echo ""
+echo -e "\e[1;30m  _____ _____            _____ _  ________ _____"
+sleep .5
+echo -e "\e[1;30m / ____|  __ \     /\   / ____| |/ /  ____|  __ \ "
+sleep .5
+echo -e "\e[1;30m| |    | |__) |   /  \ | |    | ' /| |__  | |__) |"
+sleep .5
+echo -e "\e[1;30m| |    |  _  /   / /\ \| |    |  < |  __| |  _  / "
+sleep .5
+echo -e "\e[1;30m| |____| | \ \  / ____ \ |____| . \| |____| | \ \ "
+sleep .5
+echo -e "\e[1;30m \_____|_|  \_\/_/    \_\_____|_|\_\______|_|  \_\ "
+sleep 2
+echo ""
+echo -e "\e[1;32m"
+echo "[01]HOST FUNCIONALES"
+sleep 1
+echo ""
+echo "[02]GENERADOR DE PAYLOAD"
+sleep 1
+echo ""
+echo -e "\e[1;31m[03]CUENTAS VPS"
+sleep 1
+echo ""
+echo -e "\e[1;32m[04]SPAM"
+echo ""
+sleep 1
+echo "[05]BINARIOS"
+echo ""
+sleep 1
+echo "[06]ROOT"
+echo ""
+echo ""
+sleep 2
+echo -ne "\e[1;33mELEGIR: \e[1;36m"
+read elegir
+sleep 3
+clear
+cd zzz
+bash requirement$elegir.txt
+clear
+cd ..
+bash mundonetfree.sh
+clear
+#codigo
+
